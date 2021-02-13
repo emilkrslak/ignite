a08bd336aa07a10fe3591fa17db4ed663421ebb6
emil.krslak <krslak_emil@hotmail.com>
2021-02-13 15:21:18 +0100

    Thunk + Compose Redux

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -14,4 +14,11 @@ const gamesReducer = (state={initState}, action) => {
     }   
 }
 
+//Action creater
+const fetchGames = () => {
+    return{
+        type: 'FETCH_GAMES',
+    }
+}
+
 export default gamesReducer;
